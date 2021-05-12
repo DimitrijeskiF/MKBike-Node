95ac2b5081bc64273741b3b6e984e703b5f40ef9
Filip Dimitrijeski <filipdimitrijeski7@gmail.com>
2021-05-12 18:34:27 +0200

    bug fixing fcm tokens

--- src/controllers/fcmTokens.js
+++ src/controllers/fcmTokens.js
@@ -4,6 +4,18 @@ const FcmToken = require('../models/FcmToken');
 exports.creteToken = async (req, res) => {
     const fcmToken = new FcmToken(req.body);
     try {
+        const tokens = await FcmToken.find();
+
+        const fcmTokenExists = tokens.map(token => {
+            return token.fcmToken
+        })
+
+        const exists = fcmTokenExists.includes(fcmToken.fcmToken);
+
+        if (exists) {
+            return
+        }
+
         await fcmToken.save();
         res.status(200).json({
             success: true
