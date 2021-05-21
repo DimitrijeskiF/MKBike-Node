eb00fe67a331670f6fe8bd3b9aaf6e9358c5403c
Filip Dimitrijeski <filipdimitrijeski7@gmail.com>
2021-05-21 17:04:35 +0200

    xss, limit

--- src/server.js
+++ src/server.js
@@ -11,6 +11,12 @@ const notificationRouter = require('./routers/notification');
 const fcmTokenRouter = require('./routers/fcmTokens');
 const adminRouter = require('./routers/admin');
 
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require("helmet");
+const xss = require('xss-clean');
+const hpp = require('hpp');
+const rateLimit = require('express-rate-limit');
+
 const passport = require('passport');
 const path = require('path');
 let cors = require('cors')
@@ -20,10 +26,26 @@ const port = process.env.PORT;
 
 app.use(express.json());
 app.use(cors())
+
+//Security
+app.use(mongoSanitize());
+app.use(helmet());
+app.use(xss());
+app.use(hpp())
+
+//Limiting http requests
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 100,
+  max: 1
+})
+app.use(limiter)
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(passport.initialize());
 app.use(passport.session());
 require('./auth/auth')(passport);
+
+
 
 app.use(userRouter);
 app.use(pointsRouter);
@@ -36,10 +58,10 @@ app.use(adminRouter);
 
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/index.html'));
-  })
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+})
 
 app.listen(port, () => {
-    console.log('Server is running on ' + port);
+  console.log('Server is running on ' + port);
 })
 
